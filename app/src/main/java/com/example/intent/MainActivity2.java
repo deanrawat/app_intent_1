6b1dca9c1c336e820b04d2dceda53c5cb81f983c
ankit rawat <ankitsinghrawat37@gmail.com>
2021-03-21 23:15:32 +0530

    Initial commit for first app

---
+++ app/src/main/java/com/example/intent/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+TextView t;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent i=getIntent();
+        t=findViewById(R.id.textView2);
+        t.setText(i.getStringExtra("msg1"));
+    }
+}
